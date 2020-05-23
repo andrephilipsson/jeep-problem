b85d0809c42ba4310ddc229c6fa276c4cca1388a
andrephilipsson <62261582+andrephilipsson@users.noreply.github.com>
2020-05-23 22:28:07 +0200

    Initial commit

---
+++ optimal_jeep.py
@@ -0,0 +1,37 @@
+from functools import lru_cache
+import sys
+import os
+
+# Clear the terminal window
+os.system("clear" if os.name == "posix" else "cls")
+
+try:
+    desert_length = int(sys.argv[1])
+except IndexError:  # Use default value
+    desert_length = 100
+
+try:
+    fuel_size = int(sys.argv[2])
+except IndexError:  # Use default value
+    fuel_size = 50
+
+
+@lru_cache()
+def jeep(f):
+    if f == 0:
+        return 0
+    elif f == 1:
+        return 1
+    else:
+        return jeep(f-1) + (1/(2*f-1))
+
+
+f = 0
+while True:
+    d = jeep(f) * fuel_size
+    if d >= desert_length:
+        e = d - desert_length
+        print(f"Distance: {d - e}")
+        print(f"Fuel used: {(f*fuel_size) - ((2*f-1) * e)}")
+        break
+    f += 1
